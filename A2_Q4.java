b4bbad4ecfe36a78c5cbab280babec5a188f4d3c
harshit12345677 <49156598+harshit12345677@users.noreply.github.com>
2020-11-28 18:13:11 +0530

    Update A2_Q4.java

--- A2_Q4.java
+++ A2_Q4.java
@@ -1,14 +1,17 @@
 import java.util.*;
-public class q2 {
+public class A2_Q4 
+{
 
 public static void main(String[] args) 
 {
 		int num = Integer.MAX_VALUE;
-		long long var1 = 0;
+		int  var1 = 0;
+		
 		for(int i=num;i>=1;i--)
 		{
+			int s;
 			s=i*i;
-			long long var2 = (i*(i+1))/2;
+			int t = (i*(i+1))/2;
 			if(s==t)
 			{
 				System.out.println(i);
